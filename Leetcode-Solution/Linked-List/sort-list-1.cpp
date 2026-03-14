de275299209481756ff73ce5819c0a3f36d73744
labib0x0hunter <labibfaisal450@gmail.com>
2026-03-15 01:30:39 +0600

    LeetCode-Linked List solution (15/03/2026)

---
+++ Leetcode-Solution/Linked-List/sort-list-1.cpp
@@ -0,0 +1,157 @@
+// https://leetcode.com/problems/sort-list/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define intll long long
+#define endl '\n'
+
+template<typename T>
+void PRINT(vector<T>& arr) {
+    for (int i = 0; i < int(arr.size()); i++)
+        cout << arr[i] << " \n"[i == int(arr.size()) - 1];
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* Input() {
+    int n;
+    cin >> n;
+
+    ListNode* num1 = new ListNode(-1);  // Sentinal Node
+    ListNode* temp = num1;
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        temp->next = new ListNode(val);
+        temp = temp->next;
+    }
+    temp = num1->next;
+    delete num1;
+    return temp;
+}
+
+void Delete(ListNode* head) {
+    while (head != nullptr) {
+        auto temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+void Debug(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+ListNode* getMiddle(ListNode* head) {
+    ListNode* slowPtr = head;
+    ListNode* fastPtr = head;
+
+    auto jump = [&]() -> bool {
+        if (fastPtr->next != nullptr && fastPtr->next->next != nullptr) {
+            fastPtr = fastPtr->next->next;
+            return true;
+        }
+        return false;
+    };
+
+    while (jump()) {
+        slowPtr = slowPtr->next;
+    }
+
+    auto temp = slowPtr->next;
+    slowPtr->next = nullptr;
+    slowPtr = temp;
+
+    return slowPtr;
+}
+
+ListNode* mergeSort(ListNode* head) {
+    // cout << "DIV "; Debug(head);
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
+
+    ListNode* middleNode = getMiddle(head);
+    auto left = mergeSort(head);
+    auto right = mergeSort(middleNode);
+
+    // cout << "LEFT "; Debug(left);
+    // cout << "RIGHT "; Debug(right);
+
+    ListNode* sentinalNode = new ListNode(0);
+    auto tempHead = sentinalNode;
+
+    while (left != nullptr && right != nullptr) {
+        if (left->val <= right->val) {
+            tempHead->next = left;
+            left = left->next;
+        } else {
+            tempHead->next = right;
+            right = right->next;
+        }
+        tempHead = tempHead->next;
+    }
+
+    while (left != nullptr) {
+        tempHead->next = left;
+        tempHead = tempHead->next;
+        left = left->next;
+    }
+
+    while (right != nullptr) {
+        tempHead->next = right;
+        tempHead = tempHead->next;
+        right = right->next;
+    }
+
+
+    tempHead->next = nullptr;
+    tempHead = sentinalNode->next;
+    delete sentinalNode;
+    // cout << "CONQ "; Debug(tempHead);
+    return tempHead;
+}
+
+ListNode* sortList(ListNode* head) {
+    return mergeSort(head);
+}
+
+void solution() {
+    ListNode* arr = Input();
+    Debug(arr);
+
+    Debug(sortList(arr));
+
+    Delete(arr);
+}
+
+int32_t main() {
+
+    // Test Case
+    bool TEST_CASE = !true;
+
+    ios::sync_with_stdio(false) ; cin.tie(0) ;
+
+    int t_c = 1, tt_c = 1;
+    if (TEST_CASE) cin >> t_c;
+
+    // cin.ignore(); // In Case getline(cin, s)
+
+    while (t_c--) {
+        // cout << "Case " << tt_c++ << ": ";
+        solution();
+        // if (t_c) cout << '\n';
+    }
+
+    return 0;
+}
